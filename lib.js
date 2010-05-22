410668dd8b0bbeae048ab954acf7d591743c8af7
Mauro Greco <jops@mauro-grecos-macbook.local>
2010-05-22 11:49:58 +0200

    setting up

---
+++ lib.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  // Your code here
+
+	gadgets.util.registerOnLoadHandler(makeDOMRequest);
+	
+	alert('puadre');
+
+  
+  // var p = 'puadre'
+  // $("<p></p>").text(p).appendTo("body");
+  
+});
+
+
+function makeDOMRequest() {    
+  var params = {};  
+  params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;  
+  var url = "http://www.3bmeteo.com/meteo/milano";  
+  gadgets.io.makeRequest(url, response, params);
+};
+
+function response(obj) {
+  var dom = obj.data;
+  var labels = dom.getElementsByClassName("he5");
+  for (var e in labels) {
+    var p = document.createElement("p");
+    p.innerHTML = e.innerHTML;
+  }
+};        
